3034cc85ac0ffd65c1afb393f4bdac020a019afb
Kounomiya <kounomiya@tuta.io>
2024-09-17 10:29:42 +0000

    add: check_install.py

--- sign.py
+++ sign.py
@@ -4,39 +4,40 @@ T_SECRET = 999983
 
 
 def calculate_sign(payload: dict) -> str:
-    payload.pop("sign") if payload.get("sign") is not None else ""
-    timestamp = int(payload.get("ts"))
-    payload['t_secret'] = str(timestamp % T_SECRET)
-    text = ""
-    for key, value in sorted(zip(payload.keys(), payload.values())):
-        text += key + "=" + value
-        return hashlib.md5(text.encode()).hexdigest()
+	payload.pop("sign") if payload.get("sign") is not None else ""
+	timestamp = int(payload.get("ts"))
+	payload['t_secret'] = str(timestamp % T_SECRET)
+	text = ""
+	for key, value in sorted(zip(payload.keys(), payload.values())):
+		text += key + "=" + value
+	return hashlib.md5(text.encode()).hexdigest()
 
 
 def main():
-    data = {'country': 'JP',
-            'product': 'gravity',
-            'sys_lang': 'ja',
-            'uwd': '5D40hb1RjCHfcEsKAn5pdJ7hncEmKelc2uoEMOxJtpOjDSv5ZkKnAoTM40i9ZbIW\n',
-            'app_version': '9.1.1',
-            'user_country': '',
-            'idfa': 'jFSZYjTCkDSqQ74SOmseLgNb7bxrB2ysGdiR15e+pvaNKwc2nTtdfZQM47M+qn8W\n',
-            'sign': '1b76b7234672796542618e379a76522d',
-            'sim_country': 'JP',
-            'pkg': 'anonymous.sns.community.gravity',
-            'languageV2': 'ja',
-            'referrer': 'default',
-            'app_version_code': '373',
-            'zone': '9',
-            'system_version': '9',
-            'sdk_version': '28',
-            'model': 'SM-S911x',
-            'device': 'android',
-            'brand': 'samsung',
-            'ts': '1708165999'}
-    sign = calculate_sign(data)
-    print(sign)
+	data = {
+		'country': 'US',
+		'product': 'gravity',
+		'sys_lang': 'en-US',
+		'uwd': 'ccRJop7giDOBsA+XX6BWJKvLxrQH4JHbs3xKlhabvbL2zBBoq8XnyXp0WvKeG6rR',
+		'app_version': '10.1.2',
+		'user_country': '',
+		'idfa': 'juOwtzKZsQHwMov+aUW9MQ==',
+		'sim_country': 'us',
+		'pkg': 'anonymous.sns.community.gravity',
+		'languageV2': 'ja',
+		'referrer': 'default',
+		'app_version_code': '419',
+		'zone': '0',
+		'system_version': '11',
+		'sdk_version': '30',
+		'model': 'Pixel_6',
+		'device': 'android',
+		'brand': 'Google',
+		'ts': '1726568322',
+	}
+	sign = calculate_sign(data)
+	print(sign)
 
 
 if __name__ == '__main__':
-    main()
+	main()
